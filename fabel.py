cd50f7c7027b8736a1e84bb8ca821dd00e7f4357
Fabel <daprostovseeto@gmail.com>
2019-11-28 02:14:14 +0200

    First try

--- fabel.py
+++ fabel.py
@@ -11,27 +11,26 @@ class Info:
                        'Accept': 'application/json, text/javascript, */*; q=0.01',
                        'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
                        'Accept-Encoding': 'gzip, deflate, br',
-                       'X-Request-Verification-Token': 'zc0bA9fsL8bqSsIfakvv7XY8dKKBnPi_6COrgSNSquc4S2kkLrlIvhwNseUnDuodz89zNLJGRbloUJ7kv3KdD7mt2OKfvZ2xC_Y6XD1CES1NaHIL0',
+                       'X-Request-Verification-Token': 'bUWOpt9OrQVwizrAQnVwz03RHTfO5V-Sdja8p_o65SBgouiRnjl1GSBJfgAHTa4zizQd-tgLvf0mjt00DIrVohs6zZUrCxy83SeTCU8CpRx_nRBU0',
                        'X-Requested-With': 'XMLHttpRequest',
                        'DNT': '1',
                        'Connection': 'keep-alive',
                        'Referer': 'https://www.similartech.com/dashboard/websites/analysis/url/techrepublic.com'}
-    cookies_simtech = {"__hssc": "34316798.3.1574517163585", "__hssrc": "1",
-                       "__hstc": "34316798.8cb87d47d08f59ac1974bb5ab6d40c29.1574517163583.1574517163584.1574517163584.1",
-                       "__RequestVerificationToken": "9EIWNigGFDxgsv9ilUfmsV7sZEMxfeZF_V3kC4bokhm0xWrujXY0vCTmma8Htuvn_MEzx-gZtMEFVABZx1vj0YnOqpw1",
-                       "_fbp": "fb.1.1574517160121.1765027621", "_ga": "GA1.2.197520012.1574517159", "_gat": "1",
-                       "_gid": "GA1.2.743509771.1574517159",
-                       "_lr_hb_-eiz3eg/similartech": "{\"heartbeat\":1574517264036}",
-                       "_lr_tabs_-eiz3eg/similartech": "{\"sessionID\":0,\"recordingID\":\"2-8e3a745c-0462-46a8-915a-1d36d61088cb\",\"lastActivity\":1574517264034}",
-                       "_lr_uf_-eiz3eg/similartech": "09cd1452-6296-4ba4-bde3-0e07410133b8",
-                       ".SAUTH": "F94A7E37E16EFB839775AEDB18DFA28BFAB992C35EE778CC3642DE1F19EFD20261386D84901FDC4DED2F9E92984685A7308BA7DA82D452B597DCF3190AFE1B9FAE32FDF02E0D873BCB0CEC82EB0CF827D9741782D75643502BDEA85AFAE263DB09C069A8563F5642E33FE6B586BC2210FAF110D4AF4065A70C056734864D0A2F8620A254C7754849B6078C0EA89E7D2C8385B5E6",
+    cookies_simtech = {"__hssc": "34316798.1.1574880032305", "__hssrc": "1",
+                       "__hstc": "34316798.9b887398279677df0f007dee45c44667.1574693381336.1574693381336.1574880032305.2",
+                       "__RequestVerificationToken": "imXVBryPoQaU0IF2zfna9HRWP4Ytucs-OArAxoCKd945dPG6_2wyD3jNPiQ0J_xOvRVSv6DvdOk3U5AsOgASMH20j3s1",
+                       "_fbp": "fb.1.1574693368403.1205247694", "_ga": "GA1.2.1384714940.1574693378", "_gat": "1",
+                       "_gid": "GA1.2.1229183313.1574880028",
+                       "_lr_hb_-eiz3eg/similartech": "{\"heartbeat\":1574880049408}",
+                       "_lr_tabs_-eiz3eg/similartech": "{\"sessionID\":0,\"recordingID\":\"2-85f9083d-7c5f-46f1-9bbf-74f1b7e04995\",\"lastActivity\":1574880049406}",
+                       "_lr_uf_-eiz3eg/similartech": "457dc9a2-4431-4f1d-9fc6-26dcc6d9538e",
+                       ".SAUTH": "1BC1D0E00C3F09D6C4A230616570AEF93588F7CE0291163962E9F82EBB7EB556A75181FFE61298A0485A58A7E71848D80D63C23BB3FB46FDF02CE2432335A85C6C6665E83785487A46D81DAF634781740F599A5AFBA49D0ACBCDD570CC47017B2A202D8A06599B587C220D8AC75D2341B8A3C646D75825B695ABB1D3E423AA2B7C2604DCE60902391C782B03F76EB02909AD777D",
                        "cookieconsent_status": "dismiss", "hsfirstvisit": "",
-                       "hubspotutk": "8cb87d47d08f59ac1974bb5ab6d40c29", "initialLP": "/account/register",
+                       "hubspotutk": "9b887398279677df0f007dee45c44667", "initialLP": "/account/register",
                        "initialReferrer": "",
-                       "intercom-session-v16ho3zr": "Rmw3aXdXK1FHOENCR0E4STlJT0Ird0ZOdHp0aVdjbHN6WURSRVU0Z1oydVdiblhaZE9GOXNwbTc5UFVzc3FCTS0tN0ZlKzhINXRCdkwyTUFUU1FpYUNWdz09--f81446b89213d2dc508a8583c4f808b8743666fc",
-                       "sessionLP": "/account/activate?email=mapihor937@exserver.top&token=li5JTzorTvvT_tG8yAvP0cFsHgt8bUoA9vDp6ArF0gQ1&utm_source=membership&utm_medium=email&utm_campaign=welcome&guidance=on",
-                       "sessionReferrer": "https://temp-mail.org/ru/", "SGSS": "c481914b-b42a-44f2-bfd7-8f3f1ec472af",
-                       "utm_campaign": "welcome", "utm_source": "membership"}
+                       "intercom-session-v16ho3zr": "aGRCMzRMVmdQOGtnUW9uM2RTYVNSelZSN3h3S25uNUYxMjZNNG1sRHBZOGlCRlBmWkVpRnNjNEM2NW53SGpZbS0tclBQNXdmTHNFTlA0NlNiay96Tlhmdz09--ecde84f59dae77b727fa4cbc9eefe11a20afc405",
+                       "sessionLP": "/account/register", "sessionReferrer": "",
+                       "SGSS": "a4c9f75f-42fa-4ed2-92ad-d7f9e1925a07"}
 
     base_url = 'https://www.similartech.com/api/websites/analysis?site='
     adbrainer_url = 'https://dashboard.adbrainer.com/main/dashboard?token=f0fe34639af4c2587ec33f37346e15ba'
@@ -192,7 +191,7 @@ def main():
     driver_acceptor.get(info.adbrainer_url)
     sites = read_list(path.sites_path)
     try:
-        for i in range(91803, len(sites)):
+        for i in range(len(sites)):
             print(str(i) + ' ' + info.base_url + sites[i])
             site_ads = module_similar_tech(sites[i])
             if len(site_ads) != 0 and site_ads[1] != 'none':
